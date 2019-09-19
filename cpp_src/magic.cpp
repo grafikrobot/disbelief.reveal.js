ef87a097a25b89eb0d0142ef16a02fa0cafd3def
Rene Rivera <grafikrobot@gmail.com>
2019-09-19 10:20:49 -0600

    Prep for cppcon2019 speaker content emailing.

--- cpp_src/magic.cpp
+++ cpp_src/magic.cpp
@@ -1,3 +1,9 @@
+/*
+Copyright Rene Rivera 2019
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file BOOST_SOFTWARE_LICENSE.txt or copy at
+http://www.boost.org/LICENSE_1_0.txt)
+*/
 #include "std.hpp"
 int main() {
   vector<int> cards(24); iota(cards.begin(),cards.end(),0);
--- cpp_src/std.hpp
+++ cpp_src/std.hpp
@@ -1,3 +1,9 @@
+/*
+Copyright Rene Rivera 2019
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file BOOST_SOFTWARE_LICENSE.txt or copy at
+http://www.boost.org/LICENSE_1_0.txt)
+*/
 #include <vector>
 #include <random>
 #include <iostream>
